722152c369fccf0d405a20f816da9fd90b4ed94d
= <=>
2024-04-22 12:18:46 -0400

    trying vercel deploy

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,22 @@
-import adapterGhpages from "svelte-adapter-ghpages";
+// import adapterGhpages from "svelte-adapter-ghpages";
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
+// /** @type {import('@sveltejs/kit').Config} */
+// const config = {
+//   kit: {
+//     paths: {
+//       base: "/fallout-loot-tables"
+//     },
+//     adapter: adapterGhpages({
+//       fallback: 'index.html',
+//     })
+//   }
+// };
+// export default config;
+
+import adapter from '@sveltejs/adapter-static';
+
+export default {
   kit: {
-    paths: {
-      base: "/src"
-    },
-    adapter: adapterGhpages({
-      fallback: 'index.html',
-    })
+    adapter: adapter(),
   }
 };
-export default config;
